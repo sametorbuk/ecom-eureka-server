26866e8b2e8f04368bf16c9c16b7d1ca08366c47
Samet Orbuk <orbuk60@gmail.com>
2025-02-07 11:18:10 +0300

    first commit

---
+++ src/main/java/com/ecom/euroka_server/EurokaServerApplication.java
@@ -0,0 +1,14 @@
+package com.ecom.euroka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurokaServerApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurokaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecom/euroka_server/EurokaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.euroka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurokaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
